164c00cbb2ebc6b25946d489f0c8dbf7cd1754de
Allan Brockenbrough <brockenbrough@gmail.com>
2023-09-13 11:51:22 -0400

    Addded the word "really" to the landing page.

--- src/components/users/Landingpage.js
+++ src/components/users/Landingpage.js
@@ -9,7 +9,7 @@ const Landingpage = () => {
     return (
         <Card style={{ width: '30rem' }} className="mx-2 my-2">
         <Card.Body>
-          <Card.Title>Welcome to the amazing CSC 351 Social Media App</Card.Title>
+          <Card.Title>Welcome to the really amazing CSC 351 Social Media App</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">Social media as you've never seen it before.</Card.Subtitle>
           <Card.Text>
           </Card.Text>
