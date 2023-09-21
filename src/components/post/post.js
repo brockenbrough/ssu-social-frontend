b9fff3ee1ed39d70895ac7c5ff221c7d0db7010d
Alex <s0351575@salemstate.edu>
2023-09-21 12:58:38 -0400

    Added a like counter on posts, below the data

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import moment from "moment";
 
 const Post = ({ posts, isLiked }) => {
-
+  const [likeCount, setLikeCount] = useState(null);
   const formattedDate = moment(posts.date).format("MMMM Do YYYY, h:mm:ss a");
-
+  const l = 0;
+  useEffect(() => {
+    //
+    fetch(`${process.env.REACT_APP_BACKEND_SERVER_URI}/count/likes-for-post/${posts._id}`)  //this 
+      .then((response) => response.json())
+      .then((data) => {
+       // console.log("API Response:", data); // 
+        setLikeCount(data);
+        
+      })
+      .catch((error) => {
+        console.error("Error fetching like count:", error);
+      });
+  }, [posts._id]);
+  
+  
 
   return (
     <div className="d-inline-flex p-2">
@@ -25,10 +40,13 @@ const Post = ({ posts, isLiked }) => {
             )}
           </div>
           <p>{formattedDate}</p>
+          
+            <p>{likeCount} Likes</p>
+          
           <Link
             style={{ marginRight: "1cm" }}
             to={`/updatePost/${posts._id}`}
-            className="btn btn-warning "
+            className="btn btn-warning"
           >
             Update
           </Link>
