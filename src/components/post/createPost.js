544cb38e15d1e2bba894e10ff09bd47335903fb9
chrisSmithCoding <heychrisgs@gmail.com>
2023-09-22 12:26:38 -0400

    added comments to code

--- src/components/post/createPost.js
+++ src/components/post/createPost.js
@@ -1,26 +1,27 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { Form, Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
-import getUserInfo from "../../utilities/decodeJwt";
-import { Link } from "react-router-dom";
+import axios from "axios"; // Import the axios library for making HTTP requests.
+import React, { useState, useEffect } from "react"; // Import React and its hooks.
+import { Form, Button } from "react-bootstrap"; // Import components from React Bootstrap.
+import { useNavigate } from "react-router-dom"; // Import the useNavigate hook from React Router.
+import getUserInfo from "../../utilities/decodeJwt"; // Import a utility function for decoding JWT tokens.
+import { Link } from "react-router-dom"; // Import the Link component from React Router.
 
 const CreatePost = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null); // Initialize user state to null.
   const [state, setState] = useState({
     content: "",
     username: "",
-  });
+  }); // Initialize state for content and username.
 
   useEffect(() => {
-    const currentUser = getUserInfo();
-    setUser(currentUser);
+    // Use the useEffect hook to run code after the component has rendered.
+    const currentUser = getUserInfo(); // Decode the JWT token to get user info.
+    setUser(currentUser); // Set the user state with the decoded user info.
   }, []);
-  
 
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Initialize the navigate function for routing.
 
   const handleChange = (e) => {
+    // Define a handleChange function to update the state when input fields change.
     const { name, value } = e.target;
     setState({
       ...state,
@@ -29,19 +30,19 @@ const CreatePost = () => {
   };
 
   const handleSubmit = async (e) => {
-  e.preventDefault();
-  const { content, username } = state;
-  const post = {
-    id: user.id, // Access userId directly
-    content,
-    username,
+    e.preventDefault(); // Prevent the default form submission behavior.
+    const { content, username } = state; // Destructure content and username from state.
+    const post = {
+      id: user.id, // Access userId directly from the user object.
+      content,
+      username,
+    };
+    await axios.post(`${process.env.REACT_APP_BACKEND_SERVER_URI}/posts/createPost`, post); // Send a POST request to create a new post.
+    navigate("/"); // Navigate to the root page after creating the post.
   };
-  await axios.post(`${process.env.REACT_APP_BACKEND_SERVER_URI}/posts/createPost`, post);
-  navigate("/");
-};
-
 
   if (!user) {
+    // If there is no user (not authorized), render a message and a link to login.
     return (
       <div>
         <h3>
@@ -54,7 +55,7 @@ const CreatePost = () => {
     );
   }
 
-
+  // If there is a user, render a form to create a post.
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -80,7 +81,6 @@ const CreatePost = () => {
           name="content"
           value={state.content}
           onChange={handleChange}
-          s
           style={{
             height: "3cm",
             width: "12cm",
@@ -100,4 +100,4 @@ const CreatePost = () => {
   );
 };
 
-export default CreatePost
+export default CreatePost;
