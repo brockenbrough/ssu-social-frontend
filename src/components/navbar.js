e70c06a4997a7591defa0e9e81d62b19fb77a9e1
xiomary <Xiomary331@gmail.com>
2023-09-22 09:31:36 -0400

    adding comment page to nave bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,9 +15,7 @@ export default function Navbar() {
   setUser(getUserInfo())
   }, [])
   
-  // if (!user) return null   - for now, let's show the bar even not logged in.
-  // we have an issue with getUserInfo() returning null after a few minutes
-  // it seems.
+  
   return (
     <ReactNavbar bg="dark" variant="dark">
     <Container>
