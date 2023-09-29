c95edf7f1218757d89d4c2e02ae1cb5ae2e279ad
Thomas Urbanski <tjurbanski@comcast.net>
2023-09-29 12:09:08 -0400

    Added functionality to like button -Now Creates and Sends Like to MongoDB -Now Deletes Likes from MongoDB

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import getUserInfo from '../../utilities/decodeJwt';
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import moment from "moment";
+import axios from "axios"; // Import Axios
 
 const Post = ({ posts }) => {
   const [likeCount, setLikeCount] = useState(null);
-  const [viewCount, setViewCount] = useState(null);
   const [isLiked, setIsLiked] = useState(false);
-  const formattedDate = moment(posts.date).format("MMMM Do YYYY, h:mm:ss a");
+  const formattedDate = moment(posts.date).format("MMMM Do YYYY, h:mm A");
+  const { _id: postId } = posts;
+  const [user, setUser] = useState(null);
 
+  //function to count likes on a post
   useEffect(() => {
+    const currentUser = getUserInfo(); // Decode the JWT token to get user info.
+    setUser(currentUser);
     fetch(`${process.env.REACT_APP_BACKEND_SERVER_URI}/count/likes-for-post/${posts._id}`)
       .then((response) => response.json())
       .then((data) => {
@@ -19,20 +25,55 @@ const Post = ({ posts }) => {
       .catch((error) => {
         console.error("Error fetching like count:", error);
       });
-      fetch(`${process.env.REACT_APP_BACKEND_SERVER_URI}/views/${posts._id}`)  
-      .then((response) => response.json())
-      .then((data) => {
-       console.log("API Response:", data); // 
-        setViewCount(data);
-        
-      })
-      .catch((error) => {
-        console.error("Error fetching view count:", error);
-      });
   }, [posts._id]);
   
+  //function to handle the creation of and deletion of a like.
   const handleLikeClick = () => {
-    setIsLiked(!isLiked);
+    const userId = user.id;
+    handleIsLiked();
+    if (!isLiked){
+      try {
+        axios.post(`${process.env.REACT_APP_BACKEND_SERVER_URI}/likes/like`,  {postId, userId});
+        handleIsLiked();
+      } catch (error) {
+        if (error.response && error.response.status >= 400 && error.response.status <= 500) {
+          console.error("Error liking:", error.response.data.message);
+        }
+      }
+    } else {
+      try {
+        axios.delete(`${process.env.REACT_APP_BACKEND_SERVER_URI}/likes/unLike`, {
+          data: { postId, userId }, // Use 'data' field for DELETE requests to send the payload
+        })
+          .then((response) => {
+            console.log("Unlike response:", response.data); // Log the response for debugging
+            handleIsLiked();
+          })
+          .catch((error) => {
+            console.error("Error unliking:", error);
+          });
+      } catch (error) {
+        console.error("Error unliking:", error);
+      }
+    }
+  };
+
+  //handle isLiked variable for like button
+  const handleIsLiked = async () => {
+    const userId = user.id;
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_BACKEND_SERVER_URI}/user-likes/${userId}`);
+      const userLikes = response.data;
+      const postLiked = userLikes.find((likes) => likes.postId === postId);
+  
+      if (postLiked) {
+        setIsLiked(true); // User has liked the post
+      } else {
+        setIsLiked(false); // User has not liked the post
+      }
+    } catch (error) {
+      console.error("Error checking user likes:", error);
+    }
   };
 
   return (
@@ -43,9 +84,12 @@ const Post = ({ posts }) => {
             {posts.username}
           </Link>
           <Card.Text>{posts.content}</Card.Text>
+          <Card.Text>{postId}</Card.Text>
           <div class="text-center">
             <Button
               variant={isLiked ? "danger" : "outline-danger"}
+              onPrint={handleIsLiked}
+              onMouseOver={handleIsLiked}
               onClick={handleLikeClick}
             >
               {isLiked ? 'Unlike' : 'Like'}
@@ -54,9 +98,6 @@ const Post = ({ posts }) => {
           <p>{formattedDate}</p> {/* Display formattedDate */}
           {likeCount !== null && (
             <p>{`Likes: ${likeCount}`}</p> // Display likeCount if it's not null
-          )}
-          {viewCount !== null && (
-            <p>{`Views: ${viewCount}`}</p> // Display viewCount if it's not null
           )}
           <Link
             style={{ marginRight: "1cm" }}
