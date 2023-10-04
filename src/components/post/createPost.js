760a576cbb965ca67a89ccaa5e22e3b3d5699224
Nand2004 <nandp480@gmail.com>
2023-10-04 00:11:13 -0400

    Removed Modal

--- src/components/post/createPost.js
+++ src/components/post/createPost.js
@@ -9,180 +9,103 @@ import { Link } from "react-router-dom"; // Import the Link component from React
 
 
 const CreatePost = () => {
-  const inputRef = useRef(null);
-  const [images, setImages] = useState([]);
-  const [showImageModal, setShowImageModal] = useState(false);
-
-  const [user, setUser] = useState(null);
-  const [postContent, setPostContent] = useState("");
+  const [user, setUser] = useState(null); // Initialize user state to null.
+  const [state, setState] = useState({
+    content: "",
+  }); // Initialize state for content and username.
 
   useEffect(() => {
-    const currentUser = getUserInfo();
-    setUser(currentUser);
+    // Use the useEffect hook to run code after the component has rendered.
+    const currentUser = getUserInfo(); // Decode the JWT token to get user info.
+    setUser(currentUser); // Set the user state with the decoded user info.
   }, []);
 
-  const navigate = useNavigate();
-
-  const handleImageClick = () => {
-    setShowImageModal(true);
-  };
-
-  const handleImageChange = (event) => {
-    const newFiles = event.target.files;
-    setImages((prevImages) => [...prevImages, ...newFiles]);
-  };
-
-  const handleCloseImageModal = () => {
-    setShowImageModal(false);
-  };
+  const navigate = useNavigate(); // Initialize the navigate function for routing.
 
   const handleChange = (e) => {
+    // Define a handleChange function to update the state when input fields change.
     const { name, value } = e.target;
-    setPostContent(value);
+    setState({
+      ...state,
+      [name]: value,
+    });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    if (!user) {
-      return;
-    }
-
+    const { content, username } = state;
     const post = {
-      id: user.id,
-      content: postContent,
+      id: user.id, // Access userId directly
+      content,
       username: user.username,
     };
-
-    try {
-      const formData = new FormData();
-
-      Array.from(images).forEach((file, index) => {
-        formData.append(`images[${index}]`, file);
-      });
-
-      formData.append("post", JSON.stringify(post));
-
-      await axios.post(
-        `${process.env.REACT_APP_BACKEND_SERVER_URI}/create-post-with-multiple-images`,
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-
-      navigate("/getAllPost");
-    } catch (error) {
-      console.error("Error creating post:", error);
-    }
+    await axios.post(`${process.env.REACT_APP_BACKEND_SERVER_URI}/posts/createPost`, post);
+    navigate("/getAllPost");
   };
 
+
+  if (!user) {
+    // If there is no user (not authorized), render a message and a link to login.
+    return (
+      <div>
+        <h3>
+          You are not authorized to view this page, Please Login in{" "}
+          <Link to={"/login"}>
+            <a href="#">here</a>
+          </Link>
+        </h3>
+      </div>
+    );
+  }
+
   return (
-    <>
-      {!user ? (
-        <div>
-          <h3>
-            You are not authorized to view this page. Please{" "}
-            <Link to={"/login"}>login here</Link>.
-          </h3>
-        </div>
-      ) : (
-        <Form
-          onSubmit={handleSubmit}
+    <Form
+      onSubmit={handleSubmit}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh", // Adjust as needed
+        backgroundColor: "#f6f8fa", // Light gray background color
+      }}
+    >
+      <Form.Group className="mb-3" controlId="formBasicPassword" style={{ width: "60%" }}>
+        <Form.Control
+          type="text"
+          placeholder="What's on your mind?"
+          name="content"
+          value={state.content}
+          onChange={handleChange}
           style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "100vh",
-            backgroundColor: "#f6f8fa",
+            height: "3cm",
+            width: "100%",
           }}
-        >
-          <Form.Group className="mb-3" controlId="formBasicPassword" style={{ width: "60%" }}>
-            <Form.Control
-              type="text"
-              placeholder="What's on your mind?"
-              name="content"
-              value={postContent}
-              onChange={handleChange}
-              style={{ height: "3cm", width: "100%" }}
-            />
-          </Form.Group>
+        />
+      </Form.Group>
 
-          <div
-            name="img-icon"
-            onClick={handleImageClick}
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "60%",
-            }}
-          >
-            <img
-              src="/add-img-icon.png"
-              alt="Add Image Icon"
-              style={{ width: "60px", height: "60px", marginTop: ".5cm" }}
-            />
-            <img
-              src="/addVideo.png"
-              alt="Add Video Icon"
-              style={{ width: '60px', height: '60px', marginTop: '.5cm' }}
-            />
-            <input
-              type="file"
-              ref={inputRef}
-              onChange={handleImageChange}
-              multiple
-              style={{ display: "none" }}
-            />
-          </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '60%' }}>
+        <img src="/add-img-icon.png" alt="Add Image Icon" style={{ width: '60px', height: '60px', marginTop: '.5cm' }} />
+        <img src="/addVideo.png" alt="Add Video Icon" style={{ width: '60px', height: '60px', marginTop: '.5cm' }} />
+      </div>
 
-          <Button
-            style={{
-              width: "8cm",
-              marginTop: "1cm",
-              backgroundColor: "#28a745",
-              borderColor: "#28a745",
-            }}
-            variant="primary"
-            type="submit"
-            size="lg"
-          >
-            Create Post
-          </Button>
-        </Form>
-      )}
-
-      <Modal show={showImageModal} onHide={handleCloseImageModal}>
-        <Modal.Header closeButton style={{ backgroundColor: "#007bff", color: "#fff" }}>
-          <Modal.Title>Upload Images</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <input type="file" ref={inputRef} onChange={handleImageChange} multiple />
-          {images.length > 0 && (
-            <div>
-              <p>Selected Images:</p>
-              <ul>
-                {images.map((file, index) => (
-                  <li key={index}>{file.name}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </Modal.Body>
-        <Modal.Footer style={{ backgroundColor: "#f6f8fa" }}>
-          <Button variant="secondary" onClick={handleCloseImageModal}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleImageChange}>
-            Upload
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+      <Button
+        style={{
+          width: "8cm", // Make the button wider
+          marginTop: "1cm", // Adjust the margin
+          backgroundColor: "#28a745", // Green button color
+          borderColor: "#28a745", // Green border color
+        }}
+        variant="primary"
+        type="submit"
+        size="lg" // Make the button larger
+      >
+        Create Post
+      </Button>
+    </Form>
   );
+
+
 };
 
 export default CreatePost;
