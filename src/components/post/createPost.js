81e2169977d3540820deb1c04f17bd1cc90ce7ad
Nand2004 <nandp480@gmail.com>
2023-10-04 10:54:37 -0400

    Updated the image modal.

--- src/components/post/createPost.js
+++ src/components/post/createPost.js
@@ -1,29 +1,25 @@
-import axios from "axios"; // Import the axios library for making HTTP requests.
-import React, { useState, useEffect } from "react"; // Import React and its hooks.
-import { Form, Button } from "react-bootstrap"; // Import components from React Bootstrap.
-import { useNavigate } from "react-router-dom"; // Import the useNavigate hook from React Router.
-import getUserInfo from "../../utilities/decodeJwt"; // Import a utility function for decoding JWT tokens.
-import { Link } from "react-router-dom"; // Import the Link component from React Router.
-
-
-
+import axios from "axios";
+import React, { useState, useEffect, useRef } from "react";
+import { Form, Button, Modal } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import getUserInfo from "../../utilities/decodeJwt";
+import { Link } from "react-router-dom";
 
 const CreatePost = () => {
-  const [user, setUser] = useState(null); // Initialize user state to null.
+
+  // User UseStates
+  const [user, setUser] = useState(null);
   const [state, setState] = useState({
     content: "",
-  }); // Initialize state for content and username.
-
+  });
   useEffect(() => {
-    // Use the useEffect hook to run code after the component has rendered.
-    const currentUser = getUserInfo(); // Decode the JWT token to get user info.
-    setUser(currentUser); // Set the user state with the decoded user info.
+    const currentUser = getUserInfo();
+    setUser(currentUser);
+
   }, []);
 
-  const navigate = useNavigate(); // Initialize the navigate function for routing.
-
+  const navigate = useNavigate();
   const handleChange = (e) => {
-    // Define a handleChange function to update the state when input fields change.
     const { name, value } = e.target;
     setState({
       ...state,
@@ -35,12 +31,37 @@ const CreatePost = () => {
     e.preventDefault();
     const { content, username } = state;
     const post = {
-      id: user.id, // Access userId directly
+      id: user.id,
       content,
       username: user.username,
     };
     await axios.post(`${process.env.REACT_APP_BACKEND_SERVER_URI}/posts/createPost`, post);
     navigate("/getAllPost");
+  };
+
+  // Modal UseStates
+  const inputRef = useRef(null);
+  const [images, setImages] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleImageClick = () => {
+    // Open the modal when the image is clicked
+    setShowModal(true);
+  };
+
+  const handleImageChange = (event) => {
+    const newFiles = event.target.files;
+
+    // Update the image state by appending newly selected files
+    setImages((prevImages) => [...prevImages, ...newFiles]);
+
+    // Keep the modal open to allow selecting more files
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    // Close the modal without selecting an image
+    setShowModal(false);
   };
 
 
@@ -59,53 +80,113 @@ const CreatePost = () => {
   }
 
   return (
-    <Form
-      onSubmit={handleSubmit}
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh", // Adjust as needed
-        backgroundColor: "#f6f8fa", // Light gray background color
-      }}
-    >
-      <Form.Group className="mb-3" controlId="formBasicPassword" style={{ width: "60%" }}>
-        <Form.Control
-          type="text"
-          placeholder="What's on your mind?"
-          name="content"
-          value={state.content}
-          onChange={handleChange}
-          style={{
-            height: "3cm",
-            width: "100%",
-          }}
-        />
-      </Form.Group>
-
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '60%' }}>
-        <img src="/add-img-icon.png" alt="Add Image Icon" style={{ width: '60px', height: '60px', marginTop: '.5cm' }} />
-        <img src="/addVideo.png" alt="Add Video Icon" style={{ width: '60px', height: '60px', marginTop: '.5cm' }} />
-      </div>
-
-      <Button
+    <>
+      <Form
+        onSubmit={handleSubmit}
         style={{
-          width: "8cm", // Make the button wider
-          marginTop: "1cm", // Adjust the margin
-          backgroundColor: "#28a745", // Green button color
-          borderColor: "#28a745", // Green border color
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          backgroundColor: "#f6f8fa",
         }}
-        variant="primary"
-        type="submit"
-        size="lg" // Make the button larger
       >
-        Create Post
-      </Button>
-    </Form>
+        <Form.Group
+          className="mb-3"
+          controlId="formBasicPassword"
+          style={{ width: "60%" }}
+        >
+          <Form.Control
+            type="text"
+            placeholder="What's on your mind?"
+            name="content"
+            value={state.content}
+            onChange={handleChange}
+            style={{
+              height: "3cm",
+              width: "100%",
+            }}
+          />
+        </Form.Group>
+
+        <div
+          name="img-icon"
+          onClick={handleImageClick}
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "60%",
+          }}
+        >
+          <img
+            src="/add-img-icon.png"
+            alt="Add Image Icon"
+            style={{ width: "60px", height: "60px", marginTop: ".5cm" }}
+          />
+          <img src="/addVideo.png" alt="Add Video Icon" style={{ width: '60px', height: '60px', marginTop: '.5cm' }} />
+
+          {/* Add other icons as needed */}
+          <input
+            type="file"
+            ref={inputRef}
+            onChange={handleImageChange}
+            multiple // Allow multiple file selection
+            style={{ display: "none" }}
+          />
+        </div>
+
+        <Button
+          style={{
+            width: "8cm",
+            marginTop: "1cm",
+            backgroundColor: "#28a745",
+            borderColor: "#28a745",
+          }}
+          variant="primary"
+          type="submit"
+          size="lg"
+        >
+          Create Post
+        </Button>
+      </Form>
+
+      {/* Modal for uploading images */}
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton style={{ backgroundColor: "#007bff", color: "#fff" }}>
+          <Modal.Title>Upload Images</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* Input for selecting multiple images */}
+          <input
+            type="file"
+            ref={inputRef}
+            onChange={handleImageChange}
+            multiple
+          />
+          {images.length > 0 && (
+            <div>
+              <p>Selected Images:</p>
+              <ul>
+                {images.map((file, index) => (
+                  <li key={index}>{file.name}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer style={{ backgroundColor: "#f6f8fa" }}>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleImageChange}>
+            Upload
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+    </>
   );
-
-
 };
 
 export default CreatePost;
