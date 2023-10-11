fe85d9547080221314a67d2cc83a187ea435ab1b
ElioX28 <elioaxhufka@gmail.com>
2023-10-11 11:43:52 -0400

    Profile_Likes

--- src/components/users/PrivateUserProfile.js
+++ src/components/users/PrivateUserProfile.js
@@ -10,8 +10,7 @@ import axios from "axios";
 import moment from "moment";
 import getUserInfo from "../../utilities/decodeJwt";
 import Form from "react-bootstrap/Form";
-import FollowerCount from "../following/getFollowerCount";
-import FollowingCount from "../following/getFollowingCount";
+
 
 const PrivateUserProfile = () => {
   // State for showing delete confirmation modal
@@ -23,6 +22,53 @@ const PrivateUserProfile = () => {
   const [showLogoutConfirmation, setShowLogoutConfirmation] = useState(false);
   const handleCloseLogoutConfirmation = () => setShowLogoutConfirmation(false);
   const handleShowLogoutConfirmation = () => setShowLogoutConfirmation(true);
+
+  const [showUploadModal, setShowUploadModal] = useState(false);
+  const handleCloseUploadModal = () => setShowUploadModal(false);
+  const handleShowUploadModal = () => setShowUploadModal(true);
+
+
+  const [totalLikes, setTotalLikes] = useState(0); // State to store total likes
+
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [profileImageFilename, setProfileImageFilename] = useState("");
+  const [userProfileImage, setUserProfileImage] = useState("");
+  const [followerCount, setFollowerCount] = useState(0);
+  const [followingCount, setFollowingCount] = useState(0);
+  
+  
+
+  const onFileChange = event => {
+    setSelectedFile(event.target.files[0]);
+};
+
+
+const onUpload = async () => {
+  if (selectedFile) {
+    const formData = new FormData();
+    formData.append("profileImage", selectedFile);
+
+    try {
+      console.log('Username:', username);
+      const res = await axios.post(`${process.env.REACT_APP_BACKEND_SERVER_URI}/user/updateProfileImage/${username}`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      setUserProfileImage(res.data.filePath);  // Update the profile image in the state
+    } catch (error) {
+      console.error("Error uploading profile image:", error);
+    }
+  } else {
+    console.error("No files selected");
+  }
+};
+
+
+  const profileImageUrl = profileImageFilename ? `./routes/users/user.images/image/${profileImageFilename}` : "https://robohash.org/" + Math.random() + "?set=set5";
+
 
   // Fetch the user context
   const user = useContext(UserContext);
@@ -110,9 +156,25 @@ const PrivateUserProfile = () => {
       );
     }
   };
- 
-  // Handle the deletion of a post
-  const deleteConfirm = async () => {
+  const fetchTotalLikes = useCallback(async () => {
+      try {
+        console.log(username);
+        const res = await axios.get(
+          `${process.env.REACT_APP_BACKEND_SERVER_URI}/user-totallikes/${username}`
+        );
+      setTotalLikes(res.data);
+    } catch (error) {
+      console.error("Error fetching total likes:", error);
+    }
+  }, [username]); 
+
+  useEffect(() => {
+    fetchTotalLikes();
+  }, [username, fetchTotalLikes]);
+
+  
+
+const deleteConfirm = async () => {
     if (postToDelete) {
       try {
         await axios.delete(
@@ -128,149 +190,177 @@ const PrivateUserProfile = () => {
     }
   };
 
+  useEffect(() => {
+    
+    async function fetchProfileImage() {
+      try {
+        // Get the filename associated with the user
+        const response = await axios.get(`/some-endpoint-to-get-filename/${username}`);
+        const filename = response.data.filename;
+  
+        // Now use the filename to get the image
+        const res = await axios.get(`${process.env.REACT_APP_BACKEND_SERVER_URI}/user/profileImage/${filename}`);
+        setUserProfileImage(res.data.filePath);
+      } catch (error) {
+        console.error("Error fetching profile image:", error);
+      }
+    }
+    
+  
+    fetchProfileImage();
+  }, [username]);
+  ; // Include username and fetchTotalLikes as dependencies
+
+
   return (
     <div className="container">
-      {user ? (
-        <>
-          <div className="col-md-12 text-center">
-            <h1>{user && user.username}</h1>
-            <div className="col-md-12 text-center">
-              <Image
-                roundedCircle
-                src={"https://robohash.org/" + Math.random() + "?set=set5"}
-              />
-            </div>
-            <div className="col-md-12 text-center">
-              <ul>
-                <Button onClick={followerRouteChange} variant="light">
-                  {<FollowerCount username={username} />}
-                </Button>{" "}
-                <Button onClick={followingRouteChange} variant="light">
-                  {<FollowingCount username={username} />}
-                </Button>{" "}
-                <Button variant="light">800 Likes</Button>{" "}
-              </ul>
-            </div>
-            <div className="col-md-12 text-center">
-              <>
-                <Button className="me-2" onClick={handleShowLogoutConfirmation}>
-                  Log Out
-                </Button>
-                <Modal
-                  show={showLogoutConfirmation}
-                  onHide={handleCloseLogoutConfirmation}
-                  backdrop="static"
-                  keyboard={false}
-                >
-                  <Modal.Header closeButton>
-                    <Modal.Title>Log Out</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>Are you sure you want to Log Out?</Modal.Body>
-                  <Modal.Footer>
-                    <Button variant="secondary" onClick={handleCloseLogoutConfirmation}>
-                      Close
-                    </Button>
-                    <Button variant="primary" onClick={handleLogout}>
-                      Yes
-                    </Button>
-                  </Modal.Footer>
-                </Modal>
-                <Button onClick={handleEditUser}>Edit User Information</Button>
-              </>
-            </div>
-          </div>
-          <h3 className="txt">Create Post</h3>
-  
-          <Card.Header>{user && user.username}</Card.Header>
-          <div>
-            <Row>
-              <Col xs={12} sm={4} md={4}>
-                <Image
-                  width="150"
-                  roundedCircle
-                  src={"https://robohash.org/" + Math.random()}
-                />
-              </Col>
-            </Row>
-            <Card style={{ width: "5rem" }}></Card>
-          </div>
-          <Form.Group
-            className="mb-3"
-            controlId="content"
-            style={{ width: "50rem" }}
-          >
-            <Form.Control
-              type="text"
-              placeholder="Enter post here"
-              value={form.content}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <div>
-            <Button variant="primary" type="submit" onClick={handleSubmit}>
-              Submit
-            </Button>
-          </div>
-          <div>
-            <h3>Your Posts</h3>
-            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-start', gap: '1rem' }}>
-            {posts.map((posts, index) => (
-              <div key={index}>
-                <Card
-                  style={{
-                    width: "18rem",
-                    marginTop: "1cm",
-                    marginLeft: ".5cm",
-                    background: "aliceblue",
-                  }}
-                >
-                  <Card.Body>
-                    <Card.Title>
-                      <h5>Username:</h5>
-                      <Link to={"/publicprofilepage"}>{posts.username}</Link>
-                    </Card.Title>
-                    {posts.content}
-                    <p>{moment(posts.date).format("MMMM Do YYYY, h:mm A")}</p>
-                    <Link
-                      style={{ marginRight: "1cm" }}
-                      to={`/updatePost/${posts._id}`}
-                      className="btn btn-warning "
-                    >
-                      Update
-                    </Link>
-                    <Button variant="danger" onClick={() => openDeleteModal(posts)}>
-                      Delete
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </div>
-            ))}
-            </div>
-            <Modal show={showDeleteConfirmation} onHide={handleCloseDeleteConfirmation} backdrop="static" keyboard={false}>
-              <Modal.Header closeButton>
-                <Modal.Title>Delete Confirmation</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                Are you sure you want to delete this post?
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleCloseDeleteConfirmation}>
-                  No
-                </Button>
-                <Button variant="primary" onClick={deleteConfirm}>
-                  Yes
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          </div>
-        </>
-      ) : (
-        <div className="col-md-12 text-center" style={{ fontSize: '24px', fontWeight: 'bold' }}>
-          <p>
-            Please <Link to="/login" style={{ textDecoration: 'underline' }}>log in</Link> to view your profile.
-          </p>
+      <div className="col-md-12 text-center">
+        <h1>{user && user.username}</h1>
+        <div className="col-md-12 text-center">
+        <Image roundedCircle src={profileImageFilename} />
+          <Button onClick={handleShowUploadModal}>Change Profile Picture</Button>
         </div>
-      )}
+        
+        <div className="col-md-12 text-center">
+          <ul>
+            <Button onClick={followerRouteChange} variant="light">
+              {<FollowerCount username={username} />}
+            </Button>
+            <Button onClick={followingRouteChange} variant="light">
+              {<FollowingCount username={username} />}
+            </Button>
+            <Button variant="light">{totalLikes} Likes</Button>
+          </ul>
+        </div>
+        <div className="col-md-12 text-center">
+          <Button className="me-2" onClick={handleShowLogoutConfirmation}>
+            Log Out
+          </Button>
+          <Modal
+            show={showLogoutConfirmation}
+            onHide={handleCloseLogoutConfirmation}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Log Out</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Are you sure you want to Log Out?</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleCloseLogoutConfirmation}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleLogout}>
+                Yes
+              </Button>
+            </Modal.Footer>
+          </Modal>
+          <Button onClick={handleEditUser}>Edit User Information</Button>
+        </div>
+      </div>
+
+      <h3 className="txt">Create Post</h3>
+      <Card.Header>{user && user.username}</Card.Header>
+      <div>
+        <Row>
+          <Col xs={12} sm={4} md={4}>
+          <Image roundedCircle src={profileImageUrl} />
+          </Col>
+        </Row>
+        <Card style={{ width: "5rem" }}></Card>
+      </div>
+
+      <Form.Group
+        className="mb-3"
+        controlId="content"
+        style={{ width: "50rem" }}
+      >
+        <Form.Control
+          type="text"
+          placeholder="Enter post here"
+          value={form.content}
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+
+      <h3>All Posts</h3>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-start', gap: '1rem' }}>
+        {posts.map((post, index) => (
+          <div key={index}>
+            <Card
+              style={{
+                width: "18rem",
+                marginTop: "1cm",
+                marginLeft: ".5cm",
+                background: "aliceblue",
+              }}
+            >
+              <Card.Body>
+                <Card.Title>
+                  <h5>Username:</h5>
+                  <Link to={"/publicprofilepage"}>{post.username}</Link>
+                </Card.Title>
+                {post.content}
+                <p>{moment(post.date).format("MMMM Do YYYY, h:mm A")}</p>
+                <Link
+                  style={{ marginRight: "1cm" }}
+                  to={`/updatePost/${post._id}`}
+                  className="btn btn-warning"
+                >
+                  Update
+                </Link>
+                <Button variant="danger" onClick={() => openDeleteModal(post)}>
+                  Delete
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
+
+      <Modal show={showDeleteConfirmation} onHide={handleCloseDeleteConfirmation} backdrop="static" keyboard={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Confirmation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to delete this post?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseDeleteConfirmation}>
+            No
+          </Button>
+          <Button variant="primary" onClick={deleteConfirm}>
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+              
+<Modal show={showUploadModal} onHide={handleCloseUploadModal}>
+    <Modal.Header closeButton>
+        <Modal.Title>Change Profile Picture</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+        <input type="file" onChange={onFileChange} />
+        {selectedImage && (
+            <img 
+                src={URL.createObjectURL(selectedImage)} 
+                alt="Selected Profile" 
+                style={{ width: '100%', marginTop: '10px' }} 
+            />
+        )}
+    </Modal.Body>
+    <Modal.Footer>
+        <Button variant="secondary" onClick={handleCloseUploadModal}>
+            Close
+        </Button>
+        <Button onClick={onUpload}>Upload Profile Image</Button>
+    </Modal.Footer>
+</Modal>
+
     </div>
   );
   
