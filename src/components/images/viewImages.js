ba399f149f114d88e44b42a666eabed00de21c51
Nand2004 <nandp480@gmail.com>
2023-10-13 11:38:26 -0400

    Made ViewImages Page fancy.

--- src/components/images/viewImages.js
+++ src/components/images/viewImages.js
@@ -25,23 +25,24 @@ export default function ViewImages() {
   if (!user) return (<div><h3>You are not authorized to view this page, Please Login in <Link to={'/login'}>here</Link></h3></div>);
 
   return (
-    <div>
-      <h1>View Images</h1>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1 style={{ fontSize: '28px', color: '#333' }}>View Images</h1>
 
-      <div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap' }}>
         {images.map((image, index) => (
-          <div key={index}>
-            <h5>{image.name}</h5>
-            <p>{image.desc}</p>
-            <img src={`data:${image.img.contentType};base64,${image.base64Data}`} alt={image.name} />
-          <div>
-        </div>
-          
+          <div key={index} style={{ border: '1px solid #ccc', borderRadius: '8px', margin: '10px', padding: '15px', width: '300px' }}>
+            <h5 style={{ fontSize: '18px', color: '#444' }}>Image Title: {image.name}</h5>
+            <p style={{ fontSize: '14px', color: '#666' }}>Image Description: {image.desc}</p>
+            <img
+              src={`data:${image.img.contentType};base64,${image.base64Data}`}
+              alt={image.name}
+              style={{ maxWidth: '100%', height: 'auto', marginTop: '10px' }}
+            />
           </div>
         ))}
       </div>
 
-      <hr />
+      <hr style={{ border: '1px solid #ccc', margin: '20px 0' }} />
     </div>
   );
 }
