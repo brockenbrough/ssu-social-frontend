b479bf6cf9d7a6b3aa2dbefc0632c836b4d286b4
Derek-Martineau <derekmartineau@hotmail.com>
2023-10-16 19:02:50 -0400

    frontend(chore): applied css styling for a sticky navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,33 +3,35 @@ import getUserInfo from '../utilities/decodeJwt';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import ReactNavbar from 'react-bootstrap/Navbar';
-
-// We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
 
-// Here, we display our Navbar
+// Define sticky navbar
+const stickyNavbarStyle = {
+  position: "sticky",
+  top: 0,
+  zIndex: 100,
+};
+
+//display Navbar
 export default function Navbar() {
   const [user, setUser] = useState({})
 
   useEffect(() => {
-  setUser(getUserInfo())
+    setUser(getUserInfo())
   }, [])
-  
-  
+
   return (
-    <ReactNavbar bg="dark" variant="dark">
-    <Container>
-      <Nav className="me-auto">
-        <Nav.Link href="/feed-algorithm">Feed</Nav.Link>
-        <Nav.Link href="/createpost">Create Post</Nav.Link>
-       <Nav.Link href="/getallpost">Explore</Nav.Link>
-       <Nav.Link href="/privateUserProfile">Profile</Nav.Link>
-   
-  
-
-      </Nav>
-    </Container>
-  </ReactNavbar>
-
+    <div style={stickyNavbarStyle}>
+      <ReactNavbar bg="dark" variant="dark">
+        <Container>
+          <Nav className="me-auto">
+            <Nav.Link href="/feed-algorithm">Feed</Nav.Link>
+            <Nav.Link href="/createpost">Create Post</Nav.Link>
+            <Nav.Link href="/getallpost">Explore</Nav.Link>
+            <Nav.Link href="/privateUserProfile">Profile</Nav.Link>
+          </Nav>
+        </Container>
+      </ReactNavbar>
+    </div>
   );
 }
