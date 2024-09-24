5067043db52a54bb97b59aebd6ab52832df13c0e
jacobburk <jacobburk13@gmail.com>
2024-09-24 13:45:20 -0400

    Tailwind setup and global.css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/**/**/*.{js,jsx,ts,tsx}', // Make sure this covers all your JSX/TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
