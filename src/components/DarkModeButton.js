15a2ca0b6969d5603882f6500e321c9eeeb3226f
alramirez729 <alramirez729@gmail.com>
2024-10-01 14:47:41 -0400

    FONT AWESOME EXAMPLE/DarkMode to tailwind

--- src/components/DarkModeButton.js
+++ src/components/DarkModeButton.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { useDarkMode } from './DarkModeContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'; // Import the icons
 
 function DarkModeButton() {
-  const { darkMode, toggleDarkMode } = useDarkMode(); // Change 'setDarkMode' to 'toggleDarkMode'
+  const { darkMode, toggleDarkMode } = useDarkMode();
 
   const handleClick = () => {
-    toggleDarkMode(); // Call the 'toggleDarkMode' function to switch dark mode
+    toggleDarkMode(); // Toggle dark mode
   };
 
   return (
-
-    <button onClick={handleClick} id="darkModeButton" style={{ background: 'transparent', border: 'none',}}>
-       {darkMode ? <img src="/lightbutton.png" alt="Light Mode" style={{ width: '50px', height: '50px' }} /> : <img src="/darkbutton.png" alt="Dark Mode" style={{ width: '50px', height: '50px' }} />}
+    <button onClick={handleClick} id="darkModeButton" style={{ background: 'transparent', border: 'none', cursor: 'pointer' }}>
+      {darkMode ? (
+        <FontAwesomeIcon icon={faSun} style={{ color: '#F59E0B', fontSize: '42px' }} /> // Sun icon for light mode
+      ) : (
+        <FontAwesomeIcon icon={faMoon} style={{ color: '#6B7280', fontSize: '42px' }} /> // Moon icon for dark mode
+      )}
     </button>
-
   );
 }
 
