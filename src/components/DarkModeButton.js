7688492154f487766f99da0567e695af346f4684
karanssu <s0377009salemstate@gmail.com>
2024-10-06 17:52:10 -0400

    DarkModeButton Code refactored

--- src/components/DarkModeButton.js
+++ src/components/DarkModeButton.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faMoon } from '@fortawesome/free-regular-svg-icons'; // Import the icons
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-regular-svg-icons";
 
 function DarkModeButton() {
   const [isDarkMode, setIsDarkMode] = useState(() =>
-    document.documentElement.classList.contains('dark')
+    document.documentElement.classList.contains("dark")
   );
   const toggleTailwindTheme = () => {
-    document.documentElement.classList.toggle('dark');
-    setIsDarkMode((prevMode) => !prevMode); // Update React state after DOM change
-  }
+    document.documentElement.classList.toggle("dark");
+    setIsDarkMode((prevMode) => !prevMode);
+  };
 
   const handleClick = () => {
-    //toggleDarkMode(); 
     toggleTailwindTheme();
   };
 
@@ -20,12 +19,18 @@ function DarkModeButton() {
     <button
       onClick={handleClick}
       id="darkModeButton"
-      style={{ background: 'transparent', border: 'none', cursor: 'pointer' }}
+      style={{ background: "transparent", border: "none", cursor: "pointer" }}
     >
-      {isDarkMode ? 
-      (<FontAwesomeIcon icon={faMoon} style={{ color: '#6B7280', fontSize: '28px' }} /> // Sun icon for light mode
+      {isDarkMode ? (
+        <FontAwesomeIcon
+          icon={faMoon}
+          style={{ color: "#6B7280", fontSize: "28px" }}
+        />
       ) : (
-      <FontAwesomeIcon icon={faSun} style={{ color: '#F59E0B', fontSize: '28px' }} /> // Moon icon for dark mode
+        <FontAwesomeIcon
+          icon={faSun}
+          style={{ color: "#F59E0B", fontSize: "28px" }}
+        />
       )}
     </button>
   );
