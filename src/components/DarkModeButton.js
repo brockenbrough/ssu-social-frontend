ef4f6907f149f28d8b4ca7537050d4f2a02972f9
karanssu <s0377009salemstate@gmail.com>
2024-10-06 18:53:01 -0400

    Darkmode icon changed into Toogle button

--- src/components/DarkModeButton.js
+++ src/components/DarkModeButton.js
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-regular-svg-icons";
 
 function DarkModeButton() {
-  const [isDarkMode, setIsDarkMode] = useState(() =>
-    document.documentElement.classList.contains("dark")
-  );
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const isDarkMode = document.documentElement.classList.contains("dark");
+    if (isDarkMode) setDarkMode(isDarkMode);
+  }, []);
+
   const toggleTailwindTheme = () => {
     document.documentElement.classList.toggle("dark");
-    setIsDarkMode((prevMode) => !prevMode);
+    setDarkMode((prevMode) => !prevMode);
   };
 
   const handleClick = () => {
@@ -17,21 +21,19 @@ function DarkModeButton() {
 
   return (
     <button
-      onClick={handleClick}
       id="darkModeButton"
-      style={{ background: "transparent", border: "none", cursor: "pointer" }}
+      class="inline-flex items-center cursor-pointer"
+      onClick={handleClick}
     >
-      {isDarkMode ? (
-        <FontAwesomeIcon
-          icon={faMoon}
-          style={{ color: "#6B7280", fontSize: "28px" }}
-        />
+      <span class="mr-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+        Dark Mode
+      </span>
+      {darkMode ? (
+        <input type="checkbox" value="" class="sr-only peer" checked />
       ) : (
-        <FontAwesomeIcon
-          icon={faSun}
-          style={{ color: "#F59E0B", fontSize: "28px" }}
-        />
+        <input type="checkbox" value="" class="sr-only peer" />
       )}
+      <div class="relative w-9 h-5 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
     </button>
   );
 }
