a6296717a949f5c80439d42bf7124bfacd4c1dac
karanssu <s0377009salemstate@gmail.com>
2024-10-09 01:47:43 -0400

    Code refactored

--- src/components/post/getAllPost.js
+++ src/components/post/getAllPost.js
@@ -2,8 +2,6 @@ import React from "react";
 import PostList from "./postlist";
 
 function GetAllPostsPage() {
-  // className="bg-white dark:bg-gray-800 text-gray-900 dark:text-yellow min-h-screen"
-  // className="bg-white dark:bg-gray-800 text-center"
   return (
     <div class="dark:bg-gray-800">
       <div className="pt-4 mb-3 font-bold text-3xl text-center text-grey-900 dark:text-white">
