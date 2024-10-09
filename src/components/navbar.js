d47339590d47260c12d81ad8789c5930b452b1b2
karanssu <s0377009salemstate@gmail.com>
2024-10-09 01:24:06 -0400

    Navbar styled with tailwind

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
 import getUserInfo from "../utilities/decodeJwt";
 import Image from "react-bootstrap/Image";
 import { useDarkMode } from "../components/DarkModeContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlus as createIcon } from "@fortawesome/free-solid-svg-icons";
 import CreatePost from "./post/createPost";
-import DarkModeButton from "./DarkModeButton"; // Import your Dark Mode button
+import DarkModeButton from "./DarkModeButton";
 
 export default function Navbar() {
   const [user, setUser] = useState(getUserInfo());
   const [popupShow, setPopupShow] = useState(false);
   const { darkMode } = useDarkMode();
-  const location = useLocation();
 
   useEffect(() => {
     const userInfo = getUserInfo();
@@ -18,69 +18,37 @@ export default function Navbar() {
   }, []);
 
   return (
-    <div className={`sidebar-navbar ${darkMode ? "dark" : "bg-white"}`}>
-      <div style={{ padding: "20px" }}>
+    <div className="sidebar-navbar">
+      <div>
         <Image
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXPnHm79GDZXZjpifapjAOWRsJcA_C3FgxWQLlbto&s"
           rounded
-          className="mr-2"
-          style={{ width: "60px", height: "60px", marginLeft: "30px" }}
+          className="w-16 ml-auto mr-auto mt-3"
         />
-        <span
-          className={`navbar-brand ${darkMode ? "text-light" : "text-dark"}`}
-          style={{ fontSize: "0.75rem" }}
-        >
+        <div className="font-title text-xl mt-2 mb-5 text-center font-bold text-gray-900 dark:text-white">
           SSU Social
-        </span>
+        </div>
       </div>
 
       {/* Create Post Button */}
-      <span
-        style={{
-          color: darkMode ? "#fff" : "#000",
-          cursor: "pointer",
-          marginBottom: "20px",
-          fontSize: "1.2rem",
-          marginLeft: "0.8rem",
-        }}
-      >
+      <span className="ssu-nav-link">
         <CreatePost popupShow={popupShow} setPopupShow={setPopupShow} />
         <span onClick={() => setPopupShow(true)}>
-          Create Post{" "}
-          <span style={{ fontSize: "1.0rem", marginLeft: "5px" }}>+</span>
+          <span className="mr-2">Create Post</span>
+          <FontAwesomeIcon icon={createIcon} />
         </span>
       </span>
 
       {/* Direct Links Instead of Dropdown */}
-      <div className="flex flex-col mb-4">
-        <a
-          href="/feed-algorithm"
-          className={`sidebar-item ${
-            darkMode ? "text-light" : "text-dark"
-          } no-underline`}
-          style={{ fontSize: "1.2rem" }}
-        >
-          For You
-        </a>
-        <a
-          href="/getallpost"
-          className={`sidebar-item ${
-            darkMode ? "text-light" : "text-dark"
-          } no-underline`}
-          style={{ fontSize: "1.2rem" }}
-        >
-          Discover
-        </a>
-        <a
-          href="/privateUserProfile"
-          className={`sidebar-item ${
-            darkMode ? "text-light" : "text-dark"
-          } no-underline`}
-          style={{ fontSize: "1.2rem" }}
-        >
-          Profile
-        </a>
-      </div>
+      <a href="/feed-algorithm" className="ssu-nav-link">
+        For You
+      </a>
+      <a href="/getallpost" className="ssu-nav-link">
+        Discover
+      </a>
+      <a href="/privateUserProfile" className="ssu-nav-link">
+        Profile
+      </a>
 
       {/* Dark Mode Button */}
       <div style={{ marginTop: "auto", padding: "20px" }}>
