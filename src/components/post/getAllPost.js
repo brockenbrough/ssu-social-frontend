9c63ac60cdca111658e022d45ad4482056c15d96
karanssu <s0377009salemstate@gmail.com>
2024-10-09 01:27:01 -0400

    Code format changed

--- src/components/post/getAllPost.js
+++ src/components/post/getAllPost.js
@@ -1,17 +1,23 @@
 import React from "react";
 import PostList from "./postlist";
-import { useDarkMode } from '../DarkModeContext';
+import { useDarkMode } from "../DarkModeContext";
 
 //also known as "Discover Page"
 function GetAllPostsPage() {
-    const { darkMode } = useDarkMode();
+  const { darkMode } = useDarkMode();
 
-    return (
-        <div style={{backgroundColor: darkMode ? "#000" : "#f6f8fa", color: darkMode ? "#fff" : "#000", minHeight: '100vh', }}>
-        <h2 style={{textAlign: 'center'}}>Discover</h2>
-            <PostList type="all" />
-        </div>
-    );
+  return (
+    <div
+      style={{
+        backgroundColor: darkMode ? "#000" : "#f6f8fa",
+        color: darkMode ? "#fff" : "#000",
+        minHeight: "100vh",
+      }}
+    >
+      <h2 style={{ textAlign: "center" }}>Discover</h2>
+      <PostList type="all" />
+    </div>
+  );
 }
 
 export default GetAllPostsPage;
