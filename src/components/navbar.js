78d6b47acd8dc996b4199861bf0f261a968ae303
karanssu <s0377009salemstate@gmail.com>
2024-10-09 00:45:17 -0400

    Code format changed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -24,41 +24,68 @@ export default function Navbar() {
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXPnHm79GDZXZjpifapjAOWRsJcA_C3FgxWQLlbto&s"
           rounded
           className="mr-2"
-          style={{ width: "60px", height: "60px", marginLeft: '30px' }}
+          style={{ width: "60px", height: "60px", marginLeft: "30px" }}
         />
-        <span className={`navbar-brand ${darkMode ? "text-light" : "text-dark"}`}
-        style={{ fontSize: "0.75rem" }}>
-          
+        <span
+          className={`navbar-brand ${darkMode ? "text-light" : "text-dark"}`}
+          style={{ fontSize: "0.75rem" }}
+        >
           SSU Social
         </span>
       </div>
 
       {/* Create Post Button */}
-      <span style={{ color: darkMode ? "#fff" : "#000", cursor: "pointer", marginBottom: "20px", fontSize: "1.2rem", marginLeft:"0.8rem" }}>
+      <span
+        style={{
+          color: darkMode ? "#fff" : "#000",
+          cursor: "pointer",
+          marginBottom: "20px",
+          fontSize: "1.2rem",
+          marginLeft: "0.8rem",
+        }}
+      >
         <CreatePost popupShow={popupShow} setPopupShow={setPopupShow} />
         <span onClick={() => setPopupShow(true)}>
-          Create Post <span style={{ fontSize: "1.0rem", marginLeft: "5px" }}>+</span>
+          Create Post{" "}
+          <span style={{ fontSize: "1.0rem", marginLeft: "5px" }}>+</span>
         </span>
       </span>
 
       {/* Direct Links Instead of Dropdown */}
       <div className="flex flex-col mb-4">
-  <a href="/feed-algorithm" className={`sidebar-item ${darkMode ? "text-light" : "text-dark"} no-underline`} style={{ fontSize: "1.2rem" }}>
-    For You
-  </a>
-  <a href="/getallpost" className={`sidebar-item ${darkMode ? "text-light" : "text-dark"} no-underline`} style={{ fontSize: "1.2rem" }}>
-    Discover
-  </a>
-  <a href="/privateUserProfile" className={`sidebar-item ${darkMode ? "text-light" : "text-dark"} no-underline`} style={{ fontSize: "1.2rem" }}>
-    Profile
-  </a>
-</div>
+        <a
+          href="/feed-algorithm"
+          className={`sidebar-item ${
+            darkMode ? "text-light" : "text-dark"
+          } no-underline`}
+          style={{ fontSize: "1.2rem" }}
+        >
+          For You
+        </a>
+        <a
+          href="/getallpost"
+          className={`sidebar-item ${
+            darkMode ? "text-light" : "text-dark"
+          } no-underline`}
+          style={{ fontSize: "1.2rem" }}
+        >
+          Discover
+        </a>
+        <a
+          href="/privateUserProfile"
+          className={`sidebar-item ${
+            darkMode ? "text-light" : "text-dark"
+          } no-underline`}
+          style={{ fontSize: "1.2rem" }}
+        >
+          Profile
+        </a>
+      </div>
 
       {/* Dark Mode Button */}
-      <div style={{ marginTop: 'auto', padding: '20px' }}>
+      <div style={{ marginTop: "auto", padding: "20px" }}>
         <DarkModeButton />
       </div>
     </div>
-
   );
 }
