b20ba63a42b5937d2d7a4e021933a18d5c9b8909
karanssu <s0377009salemstate@gmail.com>
2024-10-09 00:00:16 -0400

    new fonts added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./components/comments/index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './global.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./global.css";
 import { BrowserRouter } from "react-router-dom";
 import { DarkModeProvider } from "./components/DarkModeContext";
 import { TokenRefreshProvider } from "./components/contexts/tokenRefreshContext";
@@ -29,6 +29,16 @@ const Root = () => {
               integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
               crossOrigin="anonymous"
             />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossorigin
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+              rel="stylesheet"
+            />
             <App />
           </DarkModeProvider>
         </TokenRefreshProvider>
@@ -37,7 +47,4 @@ const Root = () => {
   );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.getElementById("root")
-);
+ReactDOM.render(<Root />, document.getElementById("root"));
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/**/**/*.{js,jsx,ts,tsx}', 
-    './src/**/**/*.{js,jsx,ts,tsx}',
-    './src/**/*.{js,jsx,ts,tsx}',
-    './src/*.{js,jsx,ts,tsx}',// Make sure this covers all your JSX/TSX files
+    "./src/**/**/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/*.{js,jsx,ts,tsx}", // Make sure this covers all your JSX/TSX files
   ],
   darkMode: "selector",
   theme: {
+    fontFamily: {
+      title: ["Montserrat", "sans-serif"],
+      display: ["Poppins", "sans-serif"],
+      menu: ["Roboto", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
