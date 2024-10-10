3ad0e53988668d9124bb2a81a9e734ae6046e075
haddyjatta <h_jatta@salemstate.edu>
2024-10-10 13:16:33 -0400

    Theme Color Specification

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,23 @@ module.exports = {
       title: ["Montserrat", "sans-serif"],
       display: ["Poppins", "sans-serif"],
       menu: ["Roboto", "sans-serif"],
+
+      extend: {
+        colors: {
+          darkBackground: '#171717',
+          lightBackground: '#fff7ed',
+          darkMainText: '#1d4ed8', // blue
+          lightMainText: '#422006', // dark brown
+          darkUserText: '#f5f5f5', // white
+          lightUserText: '#2563eb', // light blue
+          darkErrorMessage: '#450a0a', // red
+          lightErrorMessage: '#450a0a', // cranberry
+          iconBorderOrange: '#ea580c', // orange
+          darkHoverCream: '#f5f5dc', // cream for toggle
+          lightHoverBlue: '#2563eb', // light blue for toggle
+        },
+      },
     },
-    extend: {},
   },
   plugins: [],
-};
+ };
