ff3742d88b4ada538a366afbb41d81170a37ccab
karanssu <s0377009salemstate@gmail.com>
2024-10-22 15:15:25 -0400

    Broken import fixed

--- src/components/search/SearchResultsPosts.js
+++ src/components/search/SearchResultsPosts.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import PostList from "/Users/seanbolles/ssu_social/ssu-social-frontend/src/components/post/postlist";
+import PostList from "../post/postlist";
 
 function SearchResultsPosts() {
   const { searchInput } = useParams(); // Get the search input from URL parameters
@@ -34,7 +34,9 @@ function SearchResultsPosts() {
         Search Results for "{searchInput}"
       </div>
       {loading ? (
-        <div className="text-center text-gray-900 dark:text-white">Loading...</div>
+        <div className="text-center text-gray-900 dark:text-white">
+          Loading...
+        </div>
       ) : error ? (
         <div className="text-center text-red-500">{error}</div>
       ) : posts.length === 0 ? (
