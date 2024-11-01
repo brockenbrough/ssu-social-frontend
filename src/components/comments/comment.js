01f9e6590dbe7af8c164b8076df05b7caf2e93e1
karanssu <s0377009salemstate@gmail.com>
2024-11-01 14:33:55 -0400

    code format changed

--- src/components/comments/comment.js
+++ src/components/comments/comment.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios'
+import axios from "axios";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Stack from "react-bootstrap/Stack";
@@ -8,12 +8,15 @@ import Stack from "react-bootstrap/Stack";
 const Comment = (props) => {
   const comment = props.record;
   const deleteComment = async (comment) => {
-    axios.delete(`${process.env.REACT_APP_BACKEND_SERVER_URI}/comments/comment/${comment._id}`)
-        .then(response => {
-            alert('Comment deleted.')
-        })
-        .catch(error => alert('Error deleting comment'))
-  }
+    axios
+      .delete(
+        `${process.env.REACT_APP_BACKEND_SERVER_URI}/comments/comment/${comment._id}`
+      )
+      .then((response) => {
+        alert("Comment deleted.");
+      })
+      .catch((error) => alert("Error deleting comment"));
+  };
 
   return (
     <Card
@@ -27,26 +30,33 @@ const Comment = (props) => {
         <Stack>
           <div>
             <textarea
-                className="form-control"
-                rows="4"
-                value={comment.commentContent}
-                readOnly
-                style={{
-                  resize: "none",
-                  whiteSpace: "pre-line",
-                  overflowWrap: "break-word", // Add this line
-                }}
-              />
+              className="form-control"
+              rows="4"
+              value={comment.commentContent}
+              readOnly
+              style={{
+                resize: "none",
+                whiteSpace: "pre-line",
+                overflowWrap: "break-word", // Add this line
+              }}
+            />
           </div>
           <div>
             <Button
               variant="primary"
               className="mx-1 my-1"
-              href={`/comments/editComment/${comment._id}`}>
+              href={`/comments/editComment/${comment._id}`}
+            >
               Edit
             </Button>
-            
-            <Button variant="primary" className="mx-1 my-1" onClick={() => deleteComment(comment)}>Delete</Button>
+
+            <Button
+              variant="primary"
+              className="mx-1 my-1"
+              onClick={() => deleteComment(comment)}
+            >
+              Delete
+            </Button>
           </div>
         </Stack>
       </Card.Body>
