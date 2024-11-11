3d552901f208e00cdb32cc9b4b91ecf57a5cf7a8
karanssu <s0377009salemstate@gmail.com>
2024-11-11 01:27:04 -0500

    message mark as read feature with live update without refresh

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -153,6 +153,16 @@ const Chat = () => {
       }
     });
 
+    socket.on("messageRead", (readMessageIds) => {
+      console.log("readMessageIds", readMessageIds);
+
+      setMessages((prevMessages) =>
+        prevMessages.map((m) =>
+          readMessageIds.includes(m._id) ? { ...m, isRead: true } : m
+        )
+      );
+    });
+
     return () => {
       socket.off("message");
     };
@@ -190,6 +200,8 @@ const Chat = () => {
       await apiClient.put("/message/markAsRead", {
         messageIds: messageIds,
       });
+
+      socket.emit("messageRead", messageIds);
     } catch (error) {
       console.error("Error marking messages as read:", error);
     }
@@ -207,14 +219,6 @@ const Chat = () => {
 
     try {
       markMessagesAsReadInDb(chatRoomUnreadMessageIds);
-
-      setMessages((prevMessages) =>
-        prevMessages.map((m) =>
-          m.receiverId === user._id && m.chatRoomId === chatRoomId
-            ? { ...m, isRead: true }
-            : m
-        )
-      );
     } catch (error) {
       console.error("Error marking messages as read:", error);
     }
