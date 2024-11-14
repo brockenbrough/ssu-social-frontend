ae6c6ebb05d931d75037cf739876cf991e948b49
Josh Raimo <Joshraimo@gmail.com>
2024-11-14 14:11:29 -0500

    Removed console logs in ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -4,14 +4,12 @@ import getUserInfo from "../utilities/decodeJwt";
 
 const ProtectedRoute = () => {
   const user = getUserInfo();
-  console.log("User info:", user); // Debug log to see if user info is correctly retrieved
 
   if (!user) {
     console.log("User is not logged in, redirecting to login.");
     return <Navigate to="/" replace />;
   }
-
-  console.log("User is logged in, rendering protected route.");
+  
   return <Outlet />;
 };
 
