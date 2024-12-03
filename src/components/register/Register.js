1f7aafb5d3c576a3dc06a6d54173d5d4bc8bba59
JashATSSU <j_ash@salemstate.edu>
2024-12-03 13:40:19 -0500

    register page fixed

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -10,8 +10,8 @@ const image4 = "/4.jpg";
 const image5 = "/5.jpg";
 const image6 = "/6.jpg";
 
-const PRIMARY_COLOR = "#FFFFFF";
-const SECONDARY_COLOR = "#0c0c1f";
+const PRIMARY_COLOR = "#622A0F";
+const SECONDARY_COLOR = "#FFFFFF";
 const url = `${process.env.REACT_APP_BACKEND_SERVER_URI}/user/signup`;
 
 const Register = () => {
@@ -39,8 +39,9 @@ const Register = () => {
 
   useEffect(() => {
     const slideshowInterval = setInterval(() => {
+      setBgColor(SECONDARY_COLOR); // Reset background color
       setCurrentImageIndex(getNextImageIndex());
-    }, 5000);// 5000 milliseconds = 5 second interval
+    }, 5000); // 5 seconds interval
 
     return () => {
       clearInterval(slideshowInterval);
@@ -52,28 +53,27 @@ const Register = () => {
 
     setFieldErrors({ username: "", email: "", password: "" });
     let errors = {};
-    
-    
+
     if (!data.username) {
       errors.username = "Username is required.";
-      } else if (data.username.length < 6) {
-        errors.username = "Username must be 6 characters or longer.";
-      } else if (data.username.length > 28){
-        errors.username = "Username must be less than 28 characters."
-      }
+    } else if (data.username.length < 6) {
+      errors.username = "Username must be 6 characters or longer.";
+    } else if (data.username.length > 28) {
+      errors.username = "Username must be less than 28 characters.";
+    }
 
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!data.email) {
       errors.email = "Email is required.";
     } else if (!emailRegex.test(data.email)) {
-      errors.email = "Email address is invalid";
+      errors.email = "Email address is invalid.";
     }
     if (!data.password) {
       errors.password = "Password is required.";
     } else if (data.password.length < 8) {
       errors.password = "Password must be 8 characters or more.";
     }
-    // If there are validation errors, set them and stop submission
+
     if (Object.keys(errors).length > 0) {
       setFieldErrors(errors);
       return;
@@ -87,13 +87,6 @@ const Register = () => {
     } catch (error) {
       setLoading(false);
       if (error.response) {
-        setFieldErrors({ ...fieldErrors, username: "Username must contain 6 characters or more" });
-        console.log(error.response.data);
-        setFieldErrors({ ...fieldErrors, email: "Enter a valid email address" });
-        console.log(error.response.data);
-        setFieldErrors({ ...fieldErrors, password: "Password must be 8 characters or more" });
-        console.log(error.response.data);
-      } else {
         setError("The Username, Email, or Password you've entered is invalid. Please try again");
       }
     }
@@ -102,7 +95,7 @@ const Register = () => {
   const labelStyling = {
     color: PRIMARY_COLOR,
     fontWeight: "bold",
-    textShadow: "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000"
+    textDecoration: "none",
   };
 
   const backgroundStyling = {
@@ -113,106 +106,100 @@ const Register = () => {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    position: 'relative'
+    position: 'relative',
+  };
+
+  const containerStyle = {
+    background: "white",
+    borderRadius: "10px",
+    padding: "20px",
   };
 
   const buttonStyling = {
     background: PRIMARY_COLOR,
     borderStyle: "none",
-    color: SECONDARY_COLOR
+    color: SECONDARY_COLOR,
   };
 
   return (
-    <div style={backgroundStyling}>
-      <div>
-        <h2 className="text-center" style={{ color: PRIMARY_COLOR }}>Create an Account</h2>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicUsername">
-            <Form.Label style={labelStyling}>Username</Form.Label>
-            <Form.Control 
-            type="username" 
-            name="username" 
-            onChange={handleChange} 
-            placeholder="6-28 characters" 
-            isInvalid={!!fieldErrors.username}/>
+    <section className="vh-100">
+      <div className="container-fluid h-custom vh-100">
+        <div className="row d-flex justify-content-center align-items-center h-100" style={backgroundStyling}>
+          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+            <div style={containerStyle}>
+              <h2 className="text-center" style={{ color: PRIMARY_COLOR }}>Create an Account</h2>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicUsername">
+                  <Form.Label style={labelStyling}>Username</Form.Label>
+                  <Form.Control
+                    type="username"
+                    name="username"
+                    onChange={handleChange}
+                    placeholder="6-28 characters"
+                    isInvalid={!!fieldErrors.username}
+                  />
                   {fieldErrors.username && (
-                    <Form.Text style={{ color: '#ff9999'}}>{fieldErrors.username}</Form.Text>
+                    <Form.Text style={{ color: '#ff9999' }}>{fieldErrors.username}</Form.Text>
                   )}
-            {error.username && (
-              <Form.Control.Feedback type="invalid">
-                {error.username}
-              </Form.Control.Feedback>
-            )}
-           
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label style={labelStyling}>Email</Form.Label>
-            <Form.Control 
-            type="email" 
-            name="email" 
-            onChange={handleChange} 
-            placeholder="Enter email" 
-            isInvalid={!!fieldErrors.email}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label style={labelStyling}>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    placeholder="Enter email"
+                    isInvalid={!!fieldErrors.email}
+                  />
                   {fieldErrors.email && (
-                    <Form.Text style={{ color: '#ff9999'}}>{fieldErrors.email}</Form.Text>
+                    <Form.Text style={{ color: '#ff9999' }}>{fieldErrors.email}</Form.Text>
                   )}
-            {error.email && (
-              <Form.Control.Feedback type="invalid">
-                {error.email}
-              </Form.Control.Feedback>
-            )}
-           
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label style={labelStyling}>Password</Form.Label>
-            <Form.Control 
-            type="password" 
-            name="password" 
-            placeholder="8 characters or more" 
-            onChange={handleChange} 
-            isInvalid={!!fieldErrors.password}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label style={labelStyling}>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    placeholder="8 characters or more"
+                    onChange={handleChange}
+                    isInvalid={!!fieldErrors.password}
+                  />
                   {fieldErrors.password && (
-                    <Form.Text style={{ color: '#ff9999'}}>{fieldErrors.password}</Form.Text>
-                  )
-                  }
-            {error.password && (
-              <Form.Control.Feedback type="invalid">
-                {error.password}
-              </Form.Control.Feedback>
-            )}
-            
-          </Form.Group>
-          {error && (
-            <div className="text-danger mb-3">
-              {error}
+                    <Form.Text style={{ color: '#ff9999' }}>{fieldErrors.password}</Form.Text>
+                  )}
+                </Form.Group>
+                {error && <div style={{ color: 'red' }} className="mb-3">{error}</div>}
+                <Button
+                  variant="primary"
+                  type="submit"
+                  style={buttonStyling}
+                  className="w-100 mt-3"
+                  disabled={loading}
+                >
+                  {loading ? "Submitting..." : "Submit"}
+                </Button>
+              </Form>
             </div>
-          )}
-          <Button
-            variant="primary"
-            type="submit"
-            style={buttonStyling}
-            className="w-100 mt-3"
-            disabled={loading}
-          >
-            {loading ? "Submitting..." : "Submit"}
-          </Button>
-        </Form>
+          </div>
+          <div className="col-md-4 d-none d-md-block">
+            <div style={{ height: "100%", overflow: "hidden" }}>
+              <img
+                src={containerImages[currentImageIndex]}
+                alt="Slideshow"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  boxShadow: "4px 4px 8px rgba(0, 0, 0, 0.1)",
+                  border: "4px solid #ddd",
+                  borderRadius: "8px",
+                }}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      {/* Slideshow Container on the Side */}
-      <div className="position-absolute" style={{ top: 0, right: 0, width: '30%', height: '100%' }}>
-        <img
-          src={containerImages[currentImageIndex]}
-          alt="Slideshow"
-          style={{
-            width: '100%',
-            height: '100%',
-            objectFit: "cover",
-            boxShadow: "4px 4px 8px rgba(0, 0, 0, 0.2)",
-            borderRadius: "8px"
-          }}
-        />
-      </div>
-    </div>
+    </section>
   );
 };
 
